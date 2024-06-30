38d88916b3ac4b2745caed225938907f64ed46cf
spr9530 <145482692+spr9530@users.noreply.github.com>
2024-06-30 15:27:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,10 @@ import HomePage from './Pages/HomePage.jsx';
 import SignUp from './user/SignUp.jsx';
 import MeetingPage from './Pages/MeetingPage.jsx';
 
-const socket = io('https://collab-learn-backend-blond.vercel.app');
+const socket = io('https://collab-learn-backend-blond.vercel.app', {
+    transports: ['websocket', 'polling'],
+    withCredentials: true
+});
 
 const router = createBrowserRouter([
   {
